79d23ef276fc749bb5f8aab5ccd68a3113c47b26
Yusuf <yusufbjk__44@hotmail.com>
2023-10-13 17:54:51 +0300

    Pinned Project Section

---
+++ src/components/PinnedProject.jsx
@@ -0,0 +1,243 @@
+import { Menu, Transition } from '@headlessui/react'
+import { EllipsisVerticalIcon } from '@heroicons/react/24/outline'
+import React, { Fragment } from 'react'
+
+function PinnedProjects() {
+
+  const projects = [
+    {
+      id: 1,
+      title: 'GraphQL API',
+      initials: 'GA',
+      team: 'Engineering',
+      members: [
+        {
+          name: 'Dries Vincent',
+          handle: 'driesvincent',
+          imageUrl:
+            'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        },
+        {
+          name: 'Lindsay Walton',
+          handle: 'lindsaywalton',
+          imageUrl:
+            'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        },
+        {
+          name: 'Courtney Henry',
+          handle: 'courtneyhenry',
+          imageUrl:
+            'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        },
+        {
+          name: 'Tom Cook',
+          handle: 'tomcook',
+          imageUrl:
+            'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        },
+      ],
+      totalMembers: 12,
+      lastUpdated: 'March 17, 2020',
+      pinned: true,
+      bgColorClass: 'bg-pink-600',
+    },
+    {
+      id: 2,
+      title: 'New Benefits Plan',
+      initials: 'NB',
+      team: 'Human Resources',
+      members: [
+        {
+          name: 'Dries Vincent',
+          handle: 'driesvincent',
+          imageUrl:
+            'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        },
+        {
+          name: 'Lindsay Walton',
+          handle: 'lindsaywalton',
+          imageUrl:
+            'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        },
+        {
+          name: 'Courtney Henry',
+          handle: 'courtneyhenry',
+          imageUrl:
+            'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        },
+        {
+          name: 'Tom Cook',
+          handle: 'tomcook',
+          imageUrl:
+            'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        },
+      ],
+      totalMembers: 8,
+      lastUpdated: 'April 4, 2020',
+      pinned: true,
+      bgColorClass: 'bg-purple-600',
+    },
+    {
+      id: 3,
+      title: 'Onboarding Emails',
+      initials: 'OE',
+      team: 'Customer Success',
+      members: [
+        {
+          name: 'Dries Vincent',
+          handle: 'driesvincent',
+          imageUrl:
+            'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        },
+        {
+          name: 'Lindsay Walton',
+          handle: 'lindsaywalton',
+          imageUrl:
+            'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        },
+        {
+          name: 'Courtney Henry',
+          handle: 'courtneyhenry',
+          imageUrl:
+            'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        },
+        {
+          name: 'Tom Cook',
+          handle: 'tomcook',
+          imageUrl:
+            'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        },
+      ],
+      totalMembers: 14,
+      lastUpdated: 'March 30, 2020',
+      pinned: true,
+      bgColorClass: 'bg-yellow-500',
+    },
+    {
+      id: 4,
+      title: 'iOS App',
+      initials: 'IA',
+      team: 'Engineering',
+      members: [
+        {
+          name: 'Courtney Henry',
+          handle: 'courtneyhenry',
+          imageUrl:
+            'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        },
+        {
+          name: 'Tom Cook',
+          handle: 'tomcook',
+          imageUrl:
+            'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        },
+      ],
+      totalMembers: 2,
+      lastUpdated: 'March 30, 2020',
+      pinned: true,
+      bgColorClass: 'bg-green-500',
+    },
+    
+    // More projects...
+  ]
+  const pinnedProjects = projects.filter((project) => project.pinned)
+
+  function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+  }
+
+
+  return (
+    <div>
+        <div className="mt-6 px-4 sm:px-6 lg:px-8">
+              <h2 className="text-sm font-medium text-gray-900">Pinned Projects</h2>
+              <ul role="list" className="mt-3 grid grid-cols-1 gap-4 sm:grid-cols-2 sm:gap-6 xl:grid-cols-4">
+                {pinnedProjects.map((project) => (
+                  <li key={project.id} className="relative col-span-1 flex rounded-md shadow-sm">
+                    <div
+                      className={classNames(
+                        project.bgColorClass,
+                        'flex w-16 flex-shrink-0 items-center justify-center rounded-l-md text-sm font-medium text-white'
+                      )}
+                    >
+                      {project.initials}
+                    </div>
+                    <div className="flex flex-1 items-center justify-between truncate rounded-r-md border-b border-r border-t border-gray-200 bg-white">
+                      <div className="flex-1 truncate px-4 py-2 text-sm">
+                        <a href="#" className="font-medium text-gray-900 hover:text-gray-600">
+                          {project.title}
+                        </a>
+                        <p className="text-gray-500">{project.totalMembers} Members</p>
+                      </div>
+                      <Menu as="div" className="flex-shrink-0 pr-2">
+                        <Menu.Button className="inline-flex h-8 w-8 items-center justify-center rounded-full bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2">
+                          <span className="sr-only">Open options</span>
+                          <EllipsisVerticalIcon className="h-5 w-5" aria-hidden="true" />
+                        </Menu.Button>
+                        <Transition
+                          as={Fragment}
+                          enter="transition ease-out duration-100"
+                          enterFrom="transform opacity-0 scale-95"
+                          enterTo="transform opacity-100 scale-100"
+                          leave="transition ease-in duration-75"
+                          leaveFrom="transform opacity-100 scale-100"
+                          leaveTo="transform opacity-0 scale-95"
+                        >
+                          <Menu.Items className="absolute right-10 top-3 z-10 mx-3 mt-1 w-48 origin-top-right divide-y divide-gray-200 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                            <div className="py-1">
+                              <Menu.Item>
+                                {({ active }) => (
+                                  <a
+                                    href="#"
+                                    className={classNames(
+                                      active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                      'block px-4 py-2 text-sm'
+                                    )}
+                                  >
+                                    View
+                                  </a>
+                                )}
+                              </Menu.Item>
+                            </div>
+                            <div className="py-1">
+                              <Menu.Item>
+                                {({ active }) => (
+                                  <a
+                                    href="#"
+                                    className={classNames(
+                                      active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                      'block px-4 py-2 text-sm'
+                                    )}
+                                  >
+                                    Removed from pinned
+                                  </a>
+                                )}
+                              </Menu.Item>
+                              <Menu.Item>
+                                {({ active }) => (
+                                  <a
+                                    href="#"
+                                    className={classNames(
+                                      active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                      'block px-4 py-2 text-sm'
+                                    )}
+                                  >
+                                    Share
+                                  </a>
+                                )}
+                              </Menu.Item>
+                            </div>
+                          </Menu.Items>
+                        </Transition>
+                      </Menu>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+    </div>
+  )
+}
+
+export default PinnedProjects
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import Sidebar from '../components/Sidebar'
+import PinnedProjects from '../components/PinnedProject.jsx'
 
 function Home() {
   return (
-    <div>
+    <div className='flex'>
         <Sidebar />
+        <main className='flex-1'>
+        <div className="border-b border-gray-200 px-4 py-4 sm:flex sm:items-center sm:justify-between sm:px-6 lg:px-8">
+              <div className="min-w-0 flex-1">
+                <h1 className="text-lg font-medium leading-6 text-gray-900 sm:truncate">Home</h1>
+              </div>
+              <div className="mt-4 flex sm:ml-4 sm:mt-0">
+                <button
+                  type="button"
+                  className="sm:order-0 order-1 ml-3 inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:ml-0"
+                >
+                  Share
+                </button>
+                <button
+                  type="button"
+                  className="order-0 inline-flex items-center rounded-md bg-purple-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-purple-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-purple-600 sm:order-1 sm:ml-3"
+                >
+                  Create
+                </button>
+              </div>
+            </div>
+
+            <PinnedProjects />
+        </main>
     </div>
     
   )
