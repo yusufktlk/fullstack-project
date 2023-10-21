9dae34e272e6737afea7438dcc44b8c97c383ed0
Yusuf <yusufbjk__44@hotmail.com>
2023-10-21 20:10:45 +0300

    Updated Edit Page

--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -1,69 +1,90 @@
 import React from 'react'
 import Sidebar from '../components/Sidebar'
+import Input from '../components/Input'
+import PinnedProjects from '../components/PinnedProject'
+import Button from '../components/Button'
 
 function Edit() {
   return (
     <div className='md:flex '>
         <Sidebar />
 
-        <main className='flex flex-col m-auto  md:ml-[350px]  font-thin mt-12 md:mt-12 bg-gray-200 rounded-xl p-4 md:p-11'>
-            <h1 className='text-5xl ml-24 md:m-auto tracking-wider'>Edit the Project!</h1>
-            
-            <div className='flex flex-col font-bold gap-y-10 mt-12 md:mt-20'>
-                <div className='flex gap-x-4 items-center'>
-                    <h1 className='text-xl w-44'>Project Name:</h1>
-                    <input 
-                      placeholder=' Project name' 
-                      className='border-2 border-black rounded-lg  p-2 w-[300px] font-semibold bg-white placeholder-black ' 
+        <main className='flex-1'>
+        <div className=" px-7 py-5 ">
+            <h1 className="text-lg font-medium text-gray-900 sm:truncate">Edit</h1>            
+        </div>
+        
+        <hr />
+
+        <PinnedProjects />
+
+        <div>
+          <h1 className='bg-[#F3F4F6] mt-8 py-3 px-8 border-b-2 border-t-2 font-semibold text-[14px]'>Edit Your Project</h1>
+        </div>
+
+        <form className='flex flex-col md:flex-row gap-y-20 md:gap-y-0 md:gap-x-44 mt-12 ml-12 md:ml-20'>
+          <div className='flex flex-col gap-y-6'>
+            <h1 className='text-xl font-bold underline'>Project Elements</h1>
+              <div className='flex '>
+                    <h1 className='text-xl w-44 '>Edit Project Name:</h1>
+                    <Input 
                       onChange={(e) => props.setProjectName(e.target.value)}
-                      />   
+                      placeholder={"Project Name"}
+                      type={"text"}
+                      id={"projectName"}
+                    />
                 </div>
-                
-                <div className='flex gap-x-4 items-center'>
-                    <h1 className='text-xl w-44'>Project Initials:</h1>
-                    <input 
-                      placeholder=' Initials' 
-                      className='border-2  border-black rounded-lg  p-2 w-[300px] font-semibold bg-white placeholder-black '
+
+                <div className='flex'>
+                    <h1 className='text-xl w-44 '>Edit Project Initials:</h1>
+                    <Input 
                       onChange={(e) => props.setProjectInitials(e.target.value)}
-                      />   
+                      placeholder={"Project Initials"}
+                      type={"text"}
+                      id={"projectInitialsInput"}
+                    />
                 </div>
-                
-                <div className='flex gap-x-4 items-center'>
-                    <h1 className='text-xl w-44'>Team Name:</h1>
-                    <input 
-                      placeholder=' Team name' 
-                      className='border-2 border-black rounded-lg  p-2 w-[300px] font-semibold bg-white placeholder-black ' 
+
+                <div className='flex'>
+                    <h1 className='text-xl w-44 '>Edit Team Name:</h1>
+                    <Input 
                       onChange={(e) => props.setTeamName(e.target.value)}
-                      />   
+                      placeholder={"Team Name"}
+                      type={"text"}
+                      id={"teamNameInput"}
+                    />
                 </div>
+          </div>
 
-                <div className='flex gap-x-4 items-center'>
-                    <h1 className='text-xl w-44'>Total Members:</h1>
-                    <input 
-                      placeholder=' Total members' 
-                      className='border-2 border-black rounded-lg  p-2 w-[300px] font-semibold bg-white placeholder-black ' 
-                      onChange={(e) => props.setTotalMembers(e.target.value)}
-                      />   
-                </div>
+          <div className='flex flex-col gap-y-6'>
+            <h1 className='text-xl font-bold underline'>Member Elements</h1>
+              <div className='flex gap-x-4 items-center'>
+                  <h1 className='text-xl w-40 md:w-44'>Edit Total Members:</h1>
+                  <Input
+                   onChange={(e) => props.setTotalMembers(e.target.value)}
+                   placeholder={"Total Members"}
+                   type={"text"}
+                   id={"totalMembersInput"}
+                  /> 
+              </div>
 
-                <div className='flex gap-x-4 items-center'>
-                    <h1 className='text-xl w-44'>Members Names:</h1>
-                    <input 
-                      placeholder=' Seymen, Yusuf' 
-                      className='border-2 border-black rounded-lg   p-2 w-[300px] font-semibold bg-white placeholder-black ' 
-                      onChange={(e) => props.setTotalMembers(e.target.value)}
-                      />   
-                </div>
-
-                <button 
-                  className='bg-black w-64 h-12 m-auto rounded-xl p-2 mt-2 text-white'
-                  onClick={() => props.addProjectList()}
-                  >
-                    Edit Project
-                </button>
+              <div className='flex gap-x-4 items-center'>
+                  <h1 className='text-xl w-40 md:w-44'>Edit Members Names:</h1>
+                  <Input
+                   onChange={(e) => props.setMembers(e.target.value)}
+                   placeholder={"Seymen, Yusuf"}
+                   type={"text"}
+                   id={"memberNamesInput"} />
+              </div>
             </div>
-
-        </main>
+        </form>
+        <div className='ml-32 md:ml-[450px] mt-20 mb-8'>
+          <Button
+              buttonText={"Edit Project"}
+              // onClick={() => props.addProjectList()}
+          />
+        </div>
+      </main>
     </div>
   )
 }
