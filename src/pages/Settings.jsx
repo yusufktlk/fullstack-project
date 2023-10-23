532b2272b29cd9e87bb3d1d810284b3fb49c6f70
Yusuf <yusufbjk__44@hotmail.com>
2023-10-23 21:28:50 +0300

    Settings Page Updated

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -30,7 +30,7 @@ function Settings() {
 
             {/* Settings forms */}
             <div className="">
-              <div className="grid max-w-7xl grid-cols-1 gap-x-8 gap-y-10 px-4 py-14 sm:px-6 md:grid-cols-3 lg:px-8">
+              <div className="grid max-w-7xl grid-cols-1 gap-x-8 gap-y-10 px-4 py-4 md:py-14 sm:px-6 md:grid-cols-3 lg:px-8">
                 <div>
                   <h2 className="text-base font-semibold leading-7 text-black">Personal Information</h2>
                   <p className="mt-1 text-sm leading-6 text-gray-400">
