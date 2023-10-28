f0027f951db108c9a46a746ce5dd1d8196ae8288
Yusuf <yusufbjk__44@hotmail.com>
2023-10-28 15:29:06 +0300

    button updated

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ function Button(props) {
 
   return (
     <button 
-    className='bg-indigo-500 w-full h-12  rounded-xl p-2 text-white'
+    className='bg-indigo-500 w-full h-12  rounded-xl p-2 text-white text-[13px] font-bold'
     style={{backgroundColor: props.theme}}
     >
       {props.children}
